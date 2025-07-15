e9d7f35aab7df18e91c3da4fd86f0c301c995f57
Vladimir <102673358+existentia1ist@users.noreply.github.com>
2025-07-16 01:18:54 +0300

    Update main.tf
    Signed-off-by: Vladimir <102673358+existentia1ist@users.noreply.github.com>

--- postgres/main.tf
+++ postgres/main.tf
@@ -16,7 +16,7 @@ resource "docker_image" "this" {
 
 resource "docker_container" "this" {
   name  = var.container_name
-  image = docker_image.postgres_image.image_id
+  image = docker_image.this.image_id
 
   env = [
     "POSTGRES_DB=${var.db}",
