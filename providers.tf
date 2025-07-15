df0c516122a5d891baf4fc63c1287b94028f6db7
Vladimir <102673358+existentia1ist@users.noreply.github.com>
2025-07-16 01:09:06 +0300

    Update providers.tf
    Signed-off-by: Vladimir <102673358+existentia1ist@users.noreply.github.com>

--- providers.tf
+++ providers.tf
@@ -3,11 +3,9 @@ terraform {
   required_providers {
     docker = {
       source  = "kreuzwerker/docker"
-      version = "~> 3.6.2"
+      version = "~> 3.0"
     }
   }
 }
 
-provider "docker" {
-  host = "unix:///var/run/docker.sock"
-}
+provider "docker" {}
